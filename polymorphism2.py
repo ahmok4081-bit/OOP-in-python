dd1218319c3311c985a1108654e4fe76bd906485
sakada <ahmok4081@gmail.com>
2026-09-16 15:44:31 +0700

    polymorphism bro code

---
+++ polymorphism2.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+class shape:
+    @abstractmethod
+    def area(self):
+        pass
+class Circle(shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return 3.14 * self.radius
+        
+class Square(shape):
+    def __init__(self,side):
+        self.side = side
+    def area(self):
+        return self.side ** 2
+
+        
+class Triangle(shape):
+    def __init__(self,base,height):
+        self.base = base
+        self.height = height
+    def area(self):
+        return self.base * self.height * 0.5
+        
+# the circle has 2 form is a Circle and the shape
+shapes = [Circle(), Square(),Triangle()]
+for shape in shapes:
+    print(shape.area)
